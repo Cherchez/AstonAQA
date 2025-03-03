30e5e0be6f16f04309ff4c0e9c263c04eedb8b8c
Cherchez <Cherchezar@gmail.com>
2025-03-04 00:17:05 +0300

    Lesson 3

---
+++ src/test/java/Homework2_2.java
@@ -0,0 +1,11 @@
+
+
+public class Homework2_2 {
+    public static void main(String[] args) {
+        Product[] productArray = new Product[6];
+        productArray[0] = new Product("Grechka","21.03.2024", "BelGrechka","Republic of Belarus","2 rubles","true");
+        productArray[1] = new Product("Rice", "20.04.2023", "BelRice","Republic of Belarus","2.5 rubles","true");
+        productArray[3] = new Product("Chocolate", "19.05.2022", "HappyUganda","Republic of Uganda","2 Uganda Shilling","false");
+        productArray[4] = new Product("Tulips","18.06.2025", "HollandFlowers","Holland","2 rubles","false");
+    }
+}
