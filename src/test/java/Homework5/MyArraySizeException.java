1717cabf2ce9ae0151889a981f89e136de6eada8
Cherchez <Cherchezar@gmail.com>
2025-03-12 23:52:21 +0300

    Lesson_5

--- src/test/java/Homework5/MyArraySizeException.java
+++ src/test/java/Homework5/MyArraySizeException.java
@@ -1,46 +1,32 @@
 package Homework5;
 
 class MyArraySizeException extends Exception {
+
     public MyArraySizeException(String message) {
         super(message);
-
-        String[][] correctArray = {
-                {"11", "2", "32", "4"},
-                {"23", "10", "17", "88"},
-                {"44", "51", "11", "12"},
-                {"13", "42", "75", "16"}
-        };
-
-        String[][] incorrectArray = {
-                {"1", "2", "3"},
-                {"4", "5", "6"},
-                {"7", "8", "9"}
-        };
-
-        try {
-            checkArraySize(correctArray);
-            System.out.println("Массив правильного размера");
-        } catch (MyArraySizeException e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            checkArraySize(incorrectArray);
-            System.out.println("Массив правильного размера");
-        } catch (MyArraySizeException e) {
-            System.out.println(e.getMessage());
-        }
     }
 
-    public static void checkArraySize(String[][] array) throws MyArraySizeException {
-        if (array.length != 4) {
-            throw new MyArraySizeException("Массив должен иметь 4 строки.");
-        }
-
-        for (String[] row : array) {
-            if (row.length != 4) {
-                throw new MyArraySizeException("Каждая строка массива должна содержать 4 элемента.");
+    public static void processArray(String[][] array) {
+        try {
+            if (array.length != 4) {
+                throw new MyArraySizeException("Массив должен иметь 4 строки");
             }
+
+            for (String[] row : array) {
+                if (row.length != 4) {
+                    throw new MyArraySizeException("Каждная строка должна содержать 4 стобца");
+                }
+            }
+
+            System.out.println("Размер массива валидный");
+
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    System.out.println("Элемент в [" + i + "][" + j + "]: " + array[i][j]);
+                }
+            }
+        } catch (MyArraySizeException e) {
+            System.err.println("Ошибка: " + e.getMessage());
         }
     }
 }
