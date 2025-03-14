d46c5dc00c854cc2c26bdfb75e6d56b2b6f837b7
Cherchez <Cherchezar@gmail.com>
2025-03-14 15:26:18 +0300

    Homework 6

---
+++ src/test/java/Homework6/TelephoneDirectory.java
@@ -0,0 +1,28 @@
+package Homework6;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TelephoneDirectory {
+    private Map<String, List<String>> TelephoneDirectory;
+
+    public TelephoneDirectory() {
+        this.TelephoneDirectory = new HashMap<>();
+    }
+
+    public void add(String lastName, String phoneNumber) {
+        if (TelephoneDirectory.containsKey(lastName)) {
+            TelephoneDirectory.get(lastName).add(phoneNumber);
+        } else {
+            List<String> phoneNumbers = new ArrayList<>();
+            phoneNumbers.add(phoneNumber);
+            TelephoneDirectory.put(lastName, phoneNumbers);
+        }
+    }
+
+    public List<String> get(String lastName) {
+        return phoneBook.getOrDefault(lastName, new ArrayList<>()); // Возвращает пустой список, если фамилия не найдена
+    }
+}
